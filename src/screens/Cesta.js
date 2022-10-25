10b4489c22ad402dc2308b228e69469a4af0e547
Kievv <ievsouza622@gmail.com>
2022-10-25 03:21:39 -0300

    Commit de branch de componentes

--- src/screens/Cesta.js
+++ src/screens/Cesta.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { StyleSheet, Image, Dimensions, Text, View } from 'react-native';
+import Texto from '../components/Texto';
 import topo from '../../assets/topo.png'
 import logo from '../../assets/logo.png'
 
@@ -8,17 +9,17 @@ const width = Dimensions.get('screen').width;
 export default function Cesta(){
     return <>
      <Image source={topo} style={estilos.topo} />
-    <Text style={estilos.titulo} >Detalhes da Cesta</Text>
+    <Texto style={estilos.titulo} >Detalhes da Cesta</Texto>
 
     <View style={estilos.cesta}>
-        <Text style={estilos.nome} >Cesta de Verduras</Text>
+        <Texto style={estilos.nome} >Cesta de Verduras</Texto>
         <View style={estilos.fazenda}>
         <Image source={logo} style={estilos.imagemFazenda}/>
-        <Text style={estilos.nomeFazenda}>Jenny Jack Farm</Text>
+        <Texto style={estilos.nomeFazenda}>Jenny Jack Farm</Texto>
 
         </View>
-        <Text style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha</Text>
-        <Text style={estilos.preco}>R$ 40,00 </Text>
+        <Texto style={estilos.descricao}>Uma cesta com produtos selecionados cuidadosamente da fazenda direto para sua cozinha</Texto>
+        <Texto style={estilos.preco}>R$ 40,00 </Texto>
     </View>
     </>
 }
@@ -27,7 +28,6 @@ const estilos = StyleSheet.create({
     topo: {
         width: "100%",
         height: 578 / 768 * width,
-        fontFamily: "MontserratBold"
     },
     titulo: {
         width: "100%",
@@ -47,7 +47,7 @@ const estilos = StyleSheet.create({
         color: "#464646",
         fontSize: 26,
         lineHeight: 42,
-        fontFamily: "MontserratBold"
+        fontWeight: 'bold'
     },
     fazenda: {
         flexDirection: "row",
@@ -61,8 +61,6 @@ const estilos = StyleSheet.create({
         fontSize: 16,
         lineHeight: 26,
         marginLeft: 12,
-        fontFamily: "MontserratRegular"
-
     },
     descricao: {
         color: "#A3A3A3",
