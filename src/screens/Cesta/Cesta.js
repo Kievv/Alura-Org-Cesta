de84348c82f5a7b239fe16d34e89ab2e5d9a3537
Kievv <ievsouza622@gmail.com>
2022-10-27 03:32:00 -0300

    Aprendendo a utilizar Flatlist e Scrollview

--- src/screens/Cesta/Cesta.js
+++ src/screens/Cesta/Cesta.js
@@ -1,18 +1,30 @@
 import React from 'react';
-import { StyleSheet, View, ScrollView } from 'react-native';
+import { StyleSheet, View, ScrollView, FlatList } from 'react-native';
 import Topo from './componentes/Topo.js';
 import Detalhes from './componentes/Detalhes.js';
-import Itens from './componentes/Itens.js';
+import Item from './componentes/Item.js';
+import Texto from '../../components/Texto.js';
 
 export default function Cesta({ topo, detalhes, itens }) {
   return (
-    <ScrollView>
-      <Topo {...topo} />
-      <View style={estilos.cesta}>
-        <Detalhes {...detalhes} />
-        <Itens {...itens} />
-      </View>
-    </ScrollView>
+    <>
+      <FlatList
+        data={itens.lista}
+        renderItem={Item}
+        keyExtractor={({ nome }) => nome}
+        ListHeaderComponent={() => {
+          return (
+            <>
+              <Topo {...topo} />
+              <View style={estilos.cesta}>
+                <Detalhes {...detalhes} />
+                <Texto style={estilos.titulo}>{itens.titulo}</Texto>
+              </View>
+            </>
+          );
+        }}
+      />
+    </>
   );
 }
 
@@ -20,5 +32,13 @@ const estilos = StyleSheet.create({
   cesta: {
     paddingVertical: 8,
     paddingHorizontal: 16,
+  },
+  titulo: {
+    color: '#464646',
+    fontWeight: 'bold',
+    marginTop: 32,
+    marginBottom: 8,
+    fontSize: 20,
+    lineHeight: 32,
   },
 });
